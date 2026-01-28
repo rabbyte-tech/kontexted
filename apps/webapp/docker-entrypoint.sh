c62a794ad78d913539c02f7801c93c5690403a22
Daniel Bilek <daniel.bilek@rabbyte.tech>
2026-01-28 23:04:41 +0100

    fixed baked in local connection values in docker-entrypoint.sh

--- apps/webapp/docker-entrypoint.sh
+++ apps/webapp/docker-entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+if [ -z "${DATABASE_URL}" ]; then
+  echo "DATABASE_URL is required but not set"
+  exit 1
+fi
+
 echo "Waiting for postgres to be ready..."
-until pg_isready -h postgres -U kontexted -p 5432 > /dev/null 2>&1; do
+until pg_isready -d "${DATABASE_URL}" > /dev/null 2>&1; do
   echo "Postgres is unavailable - sleeping"
   sleep 1
 done
